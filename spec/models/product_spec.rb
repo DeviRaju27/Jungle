4cbf77a156080111f440a6c6b0e42dcfcfe88aab
Devi Raju <deviraju999@gmail.com>
2022-11-21 01:33:18 -0800

    rspec test for product params validation

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+
+    it 'is valid with all attributes' do
+    @category = Category.new
+    params = {
+      name: 'dev',
+      description: 'description',
+      quantity: 5,
+      category: @category,
+      price_cents: 1000
+    }
+    expect(Product.new(params)).to be_valid
+  end
+
+    it 'is not valid without name ' do
+      @category = Category.new
+      params = {
+        description: 'description',
+        quantity: 5,
+        category: @category,
+        price_cents: 1000
+      }
+      expect(Product.new(params)).to_not be_valid
+    end
+
+      it 'is not valid without price ' do
+        @category = Category.new
+        params = {
+          name: 'dev',
+          description: 'description',
+          quantity: 5,
+          category: @category
+        }
+        expect(Product.new(params)).to_not be_valid
+      end
+
+      it 'is not valid without category ' do
+        @category = Category.new
+        params = {
+          name: 'dev',
+          description: 'description',
+          quantity: 5,
+          price_cents: 1000
+        }
+        expect(Product.new(params)).to_not be_valid
+      end
+
+
+      it 'is not valid without quantity ' do
+        @category = Category.new
+        params = {
+          name: 'dev',
+          description: 'description',
+          category: @category,
+          price_cents: 1000
+        }
+        expect(Product.new(params)).to_not be_valid
+      end
+
+  end
+end
